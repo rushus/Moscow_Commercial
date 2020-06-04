7f33be87faeeeb10ec94c8a9c01cfc7e8b369d83
rushus <rushus99@gmail.com>
2020-06-04 03:38:22 +0300

    Лабораторная работа #2

---
+++ Commercialobj.WebService/ApplicationServices/GetCommercialobjListUseCase/GetCommercialobjListUseCaseResponse.cs
@@ -0,0 +1,15 @@
+﻿using Commercialobj.DomainObjects;
+using Commercialobj.ApplicationServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Commercialobj.ApplicationServices.GetAdmAreaListUseCase
+{
+    public class GetCommercialobjListUseCaseResponse : UseCaseResponse
+    {
+        public IEnumerable<commercialobj> Commercialobjs { get; }
+
+        public GetCommercialobjListUseCaseResponse(IEnumerable<commercialobj> commercialobjs) => Commercialobjs = commercialobjs;
+    }
+}
